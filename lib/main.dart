43fcbc6dc31a3cea14d6647b7f6b05af90430d7d
Chinmay <chinmay4052@gmail.com>
2024-03-04 23:31:13 +0530

    first

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/register.dart';
+
+import 'login.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyLogin(),
+    routes: {
+      'register': (context) => MyRegister(),
+      'login': (context) => MyLogin(),
+    },
+  ));
+}
